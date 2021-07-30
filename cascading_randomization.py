5a9d8604932f338b806b30237ebbf07c8e137603
CHSdamo <58740615+CHSdamo@users.noreply.github.com>
2021-07-30 09:22:11 +0200

    Add files via upload

---
+++ cascading_randomization.py
@@ -0,0 +1,55 @@
+import torch
+from torchvision import models
+import re
+
+device = torch.device('cuda') if torch.cuda.is_available() else torch.device('cpu')
+model = models.inception_v3(pretrained=True)
+model.to(device)
+model = model.eval()
+
+model.load_state_dict(torch.load('./models/Mixed_7c.pth'))
+
+layer_randomization_order = ['Logits', 'Mixed_7c',
+                             'Mixed_7b', 'Mixed_7a',
+                             'Mixed_6e', 'Mixed_6d',
+                             'Mixed_6c', 'Mixed_6b',
+                             'Mixed_6a', 'Mixed_5d',
+                             'Mixed_5c', 'Mixed_5b',
+                             'Conv2d_4a_3x3', 'Conv2d_3b_1x1',
+                             'Conv2d_2b_3x3', 'Conv2d_2a_3x3',
+                             'Conv2d_1a_3x3']
+
+pretrained_stat = 'models/pretrained.pth'
+if not os.path.exists(pretrained_stat):
+    torch.save(model.state_dict(), 'models/pretrained.pth')
+print(model.fc.weight)
+model.load_state_dict(torch.load('models/pretrained.pth'))
+
+
+# begin randomization
+for i, layer_name in enumerate(layer_randomization_order):
+    weight_path = './models/' + layer_name + '.pth'
+    if i == 0:
+        torch.nn.init.normal_(model.fc.weight)
+        torch.save(model.state_dict(), weight_path)
+    else:
+        if os.path.exists(weight_path):
+            model.load_state_dict(torch.load(weight_path))
+        print("Cascading reinitialization up to on layer {}".format(layer_name))
+        layer_weight_order = [model.fc.weight, model.Mixed_7c.state_dict(),
+                              model.Mixed_7b.state_dict(), model.Mixed_7a.state_dict(),
+                              model.Mixed_6e.state_dict(), model.Mixed_6d.state_dict(),
+                              model.Mixed_6c.state_dict(), model.Mixed_6b.state_dict(),
+                              model.Mixed_6a.state_dict(), model.Mixed_5d.state_dict(),
+                              model.Mixed_5b.state_dict(), model.Mixed_5b.state_dict(),
+                              model.Conv2d_4a_3x3.state_dict(), model.Conv2d_3b_1x1.state_dict(),
+                              model.Conv2d_2a_3x3.state_dict(), model.Conv2d_2a_3x3.state_dict(),
+                              model.Conv2d_1a_3x3.state_dict()]
+
+        for k, v in layer_weight_order[i].items():
+            if re.match(r'.*weight$', k):
+                torch.nn.init.normal_(v)
+
+        #print(layer_weight_order[i])
+        #torch.nn.init.normal_(layer_weight_order[i])
+        torch.save(model.state_dict(), weight_path)
---
+++ parameter_test.py
@@ -0,0 +1,127 @@
+import re
+
+import torch
+import torch.nn.functional as F
+
+from PIL import Image
+
+import os
+import json
+import numpy as np
+from matplotlib.colors import LinearSegmentedColormap
+import matplotlib.pyplot as plt
+
+import torchvision
+from torchvision import models
+from torchvision import transforms
+
+from captum.attr import Saliency
+from captum.attr import IntegratedGradients
+from captum.attr import GuidedGradCam
+from captum.attr import GuidedBackprop
+from captum.attr import InputXGradient
+from captum.attr import NoiseTunnel
+from captum.attr import DeepLift
+from captum.attr import GradientShap
+from captum.attr import visualization as viz
+
+default_cmap = LinearSegmentedColormap.from_list('custom blue',
+                                                 [(0, '#ffffff'),
+                                                  (0.25, '#000000'),
+                                                  (1, '#000000')], N=256)
+# layer_randomization_order = ['Logits', 'Mixed_7c',
+#                              'Mixed_7b', 'Mixed_7a',
+#                              'Mixed_6e', 'Mixed_6d',
+#                              'Mixed_6c', 'Mixed_6b',
+#                              'Mixed_6a', 'Mixed_5d',
+#                              'Mixed_5c', 'Mixed_5b',
+#                              'Conv2d_4a_3x3', 'Conv2d_3b_1x1',
+#                              'Conv2d_2b_3x3', 'Conv2d_2a_3x3',
+#                              'Conv2d_1a_3x3']
+
+layer_randomization_order = [
+
+
+                             'Conv2d_2b_3x3', 'Conv2d_2a_3x3',
+                             'Conv2d_1a_3x3']
+
+device = torch.device('cuda') if torch.cuda.is_available() else torch.device('cpu')
+
+labels_path = os.getenv("HOME") + '/.torch/models/imagenet_class_index.json'
+with open(labels_path) as json_data:
+    idx_to_labels = json.load(json_data)
+
+transform = transforms.Compose([
+    transforms.Resize((299, 299), antialias=True),
+    transforms.ToTensor()
+])
+
+transform_normalize = transforms.Normalize(
+    mean=[0.485, 0.456, 0.406],
+    std=[0.229, 0.224, 0.225]
+)
+
+img = Image.open('./data/demo_images/ILSVRC2012_val_00015410.JPEG')
+img_np = np.array(img)
+transformed_img = transform(img)
+# input = transformed_img / 127.5 - 1.0
+input = transform_normalize(transformed_img)
+input = input.unsqueeze(0)
+input = input.to(device)
+#print(input.shape)
+
+model = models.inception_v3()
+model.to(device)
+for layer_name in layer_randomization_order:
+    model.load_state_dict(torch.load('./models/' + layer_name + '.pth'))
+    model = model.eval()
+    output = model(input)
+    output = F.softmax(output, dim=1)
+    prediction_score, pred_label_idx = torch.topk(output, 1)
+
+    pred_label_idx.squeeze_()
+    predicted_label = idx_to_labels[str(pred_label_idx.item())][1]
+
+    saliency_methods = ['Gradient', 'Smooth Grad', 'Input X Gradient',
+                        'Guided Backpropagation', 'Integrated Gradients', 'DeepLift', 'GradientShap'] #'Guided GradCAM'
+    for i in range(len(saliency_methods)):
+        saliency_method = saliency_methods[i]
+        if saliency_method == 'Gradient':
+            saliency = Saliency(model)
+            attributions = saliency.attribute(input, target=pred_label_idx)
+        elif saliency_method == 'Smooth Grad':
+            saliency = Saliency(model)
+            noise_tunnel = NoiseTunnel(saliency)
+            attributions = noise_tunnel.attribute(input, nt_type='smoothgrad', nt_samples=10, target=pred_label_idx)
+        elif saliency_method == 'Input X Gradient':
+            input_x_gradient = InputXGradient(model)
+            attributions = input_x_gradient.attribute(input, target=pred_label_idx)
+        # elif saliency_method == 'Guided GradCAM':
+        #     guided_gradcam = GuidedGradCam(model, gc_layer)
+        #    attributions = guided_gradcam.attribute(input, target=pred_label_idx)
+        elif saliency_method == 'Guided Backpropagation':
+            guided_backprop = GuidedBackprop(model)
+            attributions = guided_backprop.attribute(input, target=pred_label_idx)
+        elif saliency_method == 'Integrated Gradients':
+            integrated_gradients = IntegratedGradients(model)
+            attributions = integrated_gradients.attribute(input, target=pred_label_idx)
+        elif saliency_method == 'DeepLift':
+            deep_lift = DeepLift(model)
+            attributions = deep_lift.attribute(input, target=pred_label_idx)
+        elif saliency_method == 'GradientShap':
+            rand_img_dist = torch.cat([input * 0, input * 1])
+            gradient_shap = GradientShap(model)
+            attributions = gradient_shap.attribute(input,
+                                                   n_samples=50,
+                                                   stdevs=0.0001,
+                                                   baselines=rand_img_dist,
+                                                   target=pred_label_idx)
+
+        _ = viz.visualize_image_attr(np.transpose(attributions.squeeze().cpu().detach().numpy(), (1, 2, 0)),
+                                     np.transpose(transformed_img.squeeze().cpu().detach().numpy(), (1, 2, 0)),
+                                     method='heat_map',
+                                     cmap=default_cmap,
+                                     show_colorbar=True,
+                                     sign='positive')
+
+        _[0].savefig('./cs/'+ layer_name +'_'+ saliency_method + '.png')
